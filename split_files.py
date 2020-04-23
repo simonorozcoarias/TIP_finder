38c05e3170c8715bbdc45ae73bacb39edcd6993b
simonOrozcoArias <simon.orozco.arias@gmail.com>
2020-04-22 19:34:22 -0500

    Update split_files.py

--- split_files.py
+++ split_files.py
@@ -1,22 +1,27 @@
-import sys
-
 if __name__ == '__main__':
-        blastfile = sys.argv[1]
-        threads = int(sys.argv[2])
+    blastfile = sys.argv[1]
+    threads = int(sys.argv[2])
 
-        # reading input blast file
-        blastf = open(blastfile, 'r')
-        openfile = blastf.readlines()
+    start = time.time()
+    # reading input blast file
+    blastf = open(blastfile, 'r')
+    openfile = blastf.readlines()
+    finish = time.time()
+    print("Read blast file done! time="+str(finish - start))
 
-        # splitting original blast file in chuncks for each thread
-        start = time.time()
-        lines_per_procs = int(len(openfile)/threads)+1
-        for th in range(threads):
-                init = th*lines_per_procs + th
-                end = (th*lines_per_procs + th) + lines_per_procs - 1
-                fileTh = open(blastfile+"."+str(th),'w')
-                for line in openfile[init:end]:
-                        fileTh.write(line)
-                fileTh.close()
-        blastf.close()
-
+    # splitting original blast file in chuncks for each thread
+    start = time.time()
+    lines_per_procs = int(len(openfile)/threads)+1
+    remain = len(openfile) % threads
+    for th in range(threads):
+        if th < remain:
+            init = th * (lines_per_procs + 1)
+            end = init + lines_per_procs + 1
+        else:
+            init = th * lines_per_procs + remain
+            end = init + lines_per_procs
+        fileTh = open(blastfile+"."+str(th),'w')
+        for line in openfile[init:end]:
+            fileTh.write(line)
+        fileTh.close()
+    blastf.close()
